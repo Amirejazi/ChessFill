b80a6850c07727c2ec0a901d710abbe04d8b2e04
amirhosein <ejaziamirhosein@gmail.com>
2022-07-20 14:24:13 -0700

    -debug

--- UI_fily/gameserverwindow.xaml.cs
+++ UI_fily/gameserverwindow.xaml.cs
@@ -299,54 +299,93 @@ namespace UI_fily
                 _63.Background = new SolidColorBrush(Color.FromRgb(241, 219, 182));
                 // button haye bala brown kam rang kamrang shodand ----------------
             }
-            _1img.Source = new BitmapImage(new Uri(rokh));
-            _2img.Source = new BitmapImage(new Uri(asb));
-            _3img.Source = new BitmapImage(new Uri(fil));
-            _4img.Source = new BitmapImage(new Uri(vazir));
-            _5img.Source = new BitmapImage(new Uri(shah));
-            _6img.Source = new BitmapImage(new Uri(fil));
-            _7img.Source = new BitmapImage(new Uri(asb));
-            _8img.Source = new BitmapImage(new Uri(rokh));
-            _9img.Source = new BitmapImage(new Uri(sarbaz));
-            _10img.Source = new BitmapImage(new Uri(sarbaz));
-            _11img.Source = new BitmapImage(new Uri(sarbaz));
-            _12img.Source = new BitmapImage(new Uri(sarbaz));
-            _13img.Source = new BitmapImage(new Uri(sarbaz));
-            _14img.Source = new BitmapImage(new Uri(sarbaz));
-            _15img.Source = new BitmapImage(new Uri(sarbaz));
-            _16img.Source = new BitmapImage(new Uri(sarbaz));
-            _49img.Source = new BitmapImage(new Uri(sarbaz2));
-            _50img.Source = new BitmapImage(new Uri(sarbaz2));
-            _51img.Source = new BitmapImage(new Uri(sarbaz2));
-            _52img.Source = new BitmapImage(new Uri(sarbaz2));
-            _53img.Source = new BitmapImage(new Uri(sarbaz2));
-            _54img.Source = new BitmapImage(new Uri(sarbaz2));
-            _55img.Source = new BitmapImage(new Uri(sarbaz2));
-            _56img.Source = new BitmapImage(new Uri(sarbaz2));
-            _57img.Source = new BitmapImage(new Uri(rokh2));
-            _58img.Source = new BitmapImage(new Uri(asb2));
-            _59img.Source = new BitmapImage(new Uri(fil2));
-            _60img.Source = new BitmapImage(new Uri(vazir2));
-            _61img.Source = new BitmapImage(new Uri(shah2));
-            _62img.Source = new BitmapImage(new Uri(fil2));
-            _63img.Source = new BitmapImage(new Uri(asb2));
-            _64img.Source = new BitmapImage(new Uri(rokh2));
+            if (optionwindow.rbtncolor == "1")
+            {
 
+                _1img.Source = new BitmapImage(new Uri(rokh_w));
+                _2img.Source = new BitmapImage(new Uri(asb_w));
+                _3img.Source = new BitmapImage(new Uri(fil_w));
+                _4img.Source = new BitmapImage(new Uri(vazir_w));
+                _5img.Source = new BitmapImage(new Uri(shah_w));
+                _6img.Source = new BitmapImage(new Uri(fil_w));
+                _7img.Source = new BitmapImage(new Uri(asb_w));
+                _8img.Source = new BitmapImage(new Uri(rokh_w));
+                _9img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _10img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _11img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _12img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _13img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _14img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _15img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _16img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _49img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _50img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _51img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _52img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _53img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _54img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _55img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _56img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _57img.Source = new BitmapImage(new Uri(rokh_b));
+                _58img.Source = new BitmapImage(new Uri(asb_b));
+                _59img.Source = new BitmapImage(new Uri(fil_b));
+                _60img.Source = new BitmapImage(new Uri(vazir_b));
+                _61img.Source = new BitmapImage(new Uri(shah_b));
+                _62img.Source = new BitmapImage(new Uri(fil_b));
+                _63img.Source = new BitmapImage(new Uri(asb_b));
+                _64img.Source = new BitmapImage(new Uri(rokh_b));
+            }
+            if (optionwindow.rbtncolor == "2")
+            {
+
+                _1img.Source = new BitmapImage(new Uri(rokh_b));
+                _2img.Source = new BitmapImage(new Uri(asb_b));
+                _3img.Source = new BitmapImage(new Uri(fil_b));
+                _4img.Source = new BitmapImage(new Uri(shah_b));
+                _5img.Source = new BitmapImage(new Uri(vazir_b));
+                _6img.Source = new BitmapImage(new Uri(fil_b));
+                _7img.Source = new BitmapImage(new Uri(asb_b));
+                _8img.Source = new BitmapImage(new Uri(rokh_b));
+                _9img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _10img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _11img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _12img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _13img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _14img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _15img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _16img.Source = new BitmapImage(new Uri(sarbaz_b));
+                _49img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _50img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _51img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _52img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _53img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _54img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _55img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _56img.Source = new BitmapImage(new Uri(sarbaz_w));
+                _57img.Source = new BitmapImage(new Uri(rokh_w));
+                _58img.Source = new BitmapImage(new Uri(asb_w));
+                _59img.Source = new BitmapImage(new Uri(fil_w));
+                _60img.Source = new BitmapImage(new Uri(vazir_w));
+                _61img.Source = new BitmapImage(new Uri(shah_w));
+                _62img.Source = new BitmapImage(new Uri(fil_w));
+                _63img.Source = new BitmapImage(new Uri(asb_w));
+                _64img.Source = new BitmapImage(new Uri(rokh_w));
+            }
 
 
             servername.Content = optionwindow.rbtnnut;
         }
-        public static string rokh { get; set; }
-        public static string asb { get; set; }
-        public static string fil { get; set; }
-        public static string vazir { get; set; }
-        public static string shah { get; set; }
-        public static string sarbaz { get; set; }
-        public static string rokh2 { get; set; }
-        public static string asb2 { get; set; }
-        public static string fil2 { get; set; }
-        public static string vazir2 { get; set; }
-        public static string shah2 { get; set; }
-        public static string sarbaz2 { get; set; }
+        public static string rokh_b { get; set; }
+        public static string asb_b { get; set; }
+        public static string fil_b { get; set; }
+        public static string vazir_b { get; set; }
+        public static string shah_b { get; set; }
+        public static string sarbaz_w { get; set; }
+        public static string rokh_w { get; set; }
+        public static string asb_w { get; set; }
+        public static string fil_w { get; set; }
+        public static string vazir_w { get; set; }
+        public static string shah_w { get; set; }
+        public static string sarbaz_b { get; set; }
     }
 }
