66b9594e692dc4a03b5e80dccc8cd22506299cde
amirhosein <ejaziamirhosein@gmail.com>
2022-07-20 05:12:17 -0700

    -etesal server &cllient

--- UI_fily/portipclientwindow.xaml.cs
+++ UI_fily/portipclientwindow.xaml.cs
@@ -26,17 +26,88 @@ namespace UI_fily
         {
             InitializeComponent();
         }
+        public  string Pas { get; set; }
 
         private void btnstartclient_Click(object sender, RoutedEventArgs e)
         {
-            gameclientwindow gameclientwindow = new gameclientwindow();
-            this.Close();
-            gameclientwindow.ShowDialog();
+            if (Pas == Pass.Text)
+            {
+                gameclientwindow gameclientwindow = new gameclientwindow();
+                this.Close();
+                gameclientwindow.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("hammalak");
+            }
         }
 
-        private void btnconnectclient_Click(object sender, RoutedEventArgs e)
+        private async void btnconnectclient_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                //ConnectBtnEnable(false);
+                //ChangeState("InitClient...", new SolidColorBrush(Colors.Orange));
+                _client = new ClientStartup(int.Parse(Port.Text), Ip.Text, connectcallbak1);
+                _client.InitClient();
+                //ChangeState("Connecting to server...", new SolidColorBrush(Colors.Orange));
+                await _client.ConnectAsync();
+            }
+            catch (Exception ex)
+            {
+                //ChangeState("Connected to server...", new SolidColorBrush(Colors.Red));
+                MessageBox.Show(ex.Message + "\n try again", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //ConnectBtnEnable(true);
 
+            }
+        }
+        private async void connectcallbak1(string error)
+        {
+            if (string.IsNullOrEmpty(error))
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    //ChangeState(" Connected to server...", new SolidColorBrush(Colors.Green));
+                });
+                _transmission = new TransmissionClient(_client.ClientSocket, recieveCallback);
+                this.Dispatcher.Invoke(() =>
+                {
+                    //SendBtn.IsEnabled = true;
+                });
+                await _transmission.RecieveAsync();
+            }
+            else
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    //ChangeState("not connect...", new SolidColorBrush(Colors.Red));
+                    MessageBox.Show(error + "\ntry again", "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    //ConnectBtnEnable(true);
+                });
+            }
+        }
+
+        private void recieveCallback(string recieveMessage, string error)
+        {
+            if (string.IsNullOrEmpty(error))
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    Pas = recieveMessage;
+                });
+            }
+            else
+            {
+                _client.Close();
+                this.Dispatcher.Invoke(() =>
+                {
+                    //ChangeState("not connect...", new SolidColorBrush(Colors.Red));
+                    //ConnectBtnEnable(true);
+                    MessageBox.Show(error, "Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    //SendBtn.IsEnabled = false;
+                    _transmission = null;
+                });
+            }
         }
     }
 }
